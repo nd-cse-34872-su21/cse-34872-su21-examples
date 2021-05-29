35e0e197db667c72d758b34cf9ed1045d3db013f
Peter Bui <pbui@nd.edu>
2021-05-29 14:06:25 -0400

    Lecture 04: add Exercise 04-C

---
+++ lecture04/exercise04-C/template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+
+# Functions
+
+def find_maximum_contiguous_array(array):
+    return 0 
+        
+# Main execution
+
+def main():
+    for line in sys.stdin:
+        digits = map(int, line.split())
+        print(find_maximum_contiguous_array(digits))
+
+if __name__ == '__main__':
+    main()
