9ff301ccac6d2a9c8400f210e10c1317047e5580
Peter Bui <pbui@nd.edu>
2021-05-29 10:37:54 -0400

    Lecture 03: add Exercise 03-C

---
+++ lecture03/exercise03-C/template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Functions
+
+def count_colors(line):
+    counts = [0, 0, 0]
+    # TODO: read and count colors
+
+# Main Execution
+
+def main():
+    for line in sys.stdin:
+        counts = count_colors(line)
+        # TODO: print colors
+
+if __name__ == '__main__':
+    main()
