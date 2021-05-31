b168221572158039fde56cd8daa2a3798ac93623
Peter Bui <pbui@nd.edu>
2021-05-31 10:10:19 -0400

    Lecture 05: make permutations a generator

--- lecture05/permutations_backtracking.py
+++ lecture05/permutations_backtracking.py
@@ -3,14 +3,26 @@
 # Functions
 
 def permutations(p, c):
+    '''
+    p:  Current permutation
+    c:  Set of candidates
+    '''
+    # Base case: no more candidates, so yield permutation
     if not c:
-        print(p)
-        return
+        yield p
 
-    for e in sorted(c):
-        permutations(p + e, c.difference({e}))
+    # Recursive case
+    else:
+        # For each candidate, add it to permutation and remove it from
+        # candidates
+        for e in sorted(c):
+            yield from permutations(p + e, c.difference({e}))
 
 # Main Execution
 
+def main():
+    for permutation in permutations('', set('ABC')):
+        print(permutation)
+
 if __name__ == '__main__':
-    permutations('', set('ABC'))
+    main()
