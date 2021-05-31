c153a7e144189dd39a29b70e97877b7045dcd6c2
Peter Bui <pbui@nd.edu>
2021-05-31 09:56:37 -0400

    Lecture 05: add recursive version of subsets.py

--- lecture05/subsets.py
+++ lecture05/subsets.py
@@ -6,6 +6,27 @@ import itertools
 
 NUMBERS = range(0, 10)
 
+# Functions
+
+def subsets(s, c, k):
+    '''
+    s:  current subset
+    c:  all candidate elements
+    k:  index of current candidate element
+    '''
+
+    # Base case: we have exhausted all candidates
+    if k == len(c):
+        yield 1 if sum(s) % 3 == 0 else 0
+
+    # Recursive cases
+    else:
+        # Recurse without using current element
+        yield from subsets(s, c, k + 1)
+
+        # Recurse while using current element
+        yield from subsets(s + [c[k]], c, k + 1)
+
 # Main Execution
 
 def main():
@@ -15,6 +36,9 @@ def main():
             if sum(subset) % 3 == 0:
                 count += 1
 
+    print(count)
+
+    count = sum(subsets([], NUMBERS, 0))
     print(count)
 
 if __name__ == '__main__':
